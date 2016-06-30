10f372b3086cbde4c8a58ddea2734f016e650ff5
khang94 <khang.nm94@gmail.com>
2016-06-30 15:45:35 +0700

    Update and rename TestAdd1.js to TestAdd.js

--- TestAdd.js
+++ TestAdd.js
@@ -34,28 +34,6 @@ describe(Add, function(){
           expect(document.getElementById('result').innerHTML).toBe('3');
     });
 });
-/*describe('calculator', function () {
-
-    beforeEach(module('calculatorApp'));
-
-    var $controller;
-
-    beforeEach(inject(function(_$controller_){
-        $controller = _$controller_;
-    }));
-
-    describe('sum', function () {
-        it('1 + 1 should equal 2', function () {
-            var $scope = {};
-            var controller = $controller('CalculatorController', { $scope: $scope });
-            $scope.x = 1;
-            $scope.y = 2;
-            $scope.sum();
-            expect($scope.z).toBe(5);
-        });
-    });
-
-}); */
 /**
  * Created by khang on 6/28/2016.
  */
